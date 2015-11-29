deb6eaf93ef3ea77b2cd067376ca259d6e601311
tjdrms <tjdrms1314@gmail.com>
2015-11-29 04:55:58 +0000

    aboutus.php added (page that will introduce us as a team / individual) (Only name & email added, More info should be added later)

---
+++ aboutus.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>TableMe</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" type="text/css" media="all" href="css/bootstrap-responsive.min.css"> 
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+
+    <style>
+        h1 {
+            text-align: center;
+        }
+        h2 {
+            text-align: center;
+        }
+        .member {
+            text-align: center;
+        }
+        h5{
+            font-weight: bold;
+        }
+    </style>
+</head>
+
+<body>
+    <?php
+    include 'header.php';
+    ?>
+    <h1>SFSU CSC 648 Software Engineering Group Project, Fall 2015</h1>
+    <h2>Team 11 Members</h2>
+
+    <div class="member">
+        <div class="col-md-6 backend">
+            <h3>Back-end developers:</h3> <br>
+            <h4>Pooja Kanchan [Team Lead]</h4>
+            <p>pooj.kanchan@gmail.com</p>
+            <h4>Adrian Lee [Tech Lead]</h4>
+            <p>aznanimekid@gmail.com</p>
+            <h4>HaiChuan Duan [Back-end Lead]</h4>
+            <p>haichuand@gmail.com</p>
+        </div>
+        <div class="col-md-6 frontend">
+            <h3>Front-end developers:</h3> <br>
+            <h4>Sabreen Michael [Documentation Lead]</h4>
+            <p>sabreenmichael@gmail.com</p>
+            <h4>Michael Lee [QA Lead]</h4>
+            <p>openmichael65@gmail.com</p>
+            <h4>SeungKeun Kim [Front-end Lead]</h4>
+            <p> tjdrms1314@gmail.com </p>
+        </div>
+    </div>
+
+</body>

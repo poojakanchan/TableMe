bc86c29cc26e4a8ce455b76dbd5c9b3794208e2e
mlee8 <mlee8@42e09f76-fe15-49be-bbc6-42522261d228>
2015-12-12 05:49:17 +0000

    Fix all the links in header

--- branches/m4/app/views/home/header.php
+++ branches/m4/app/views/home/header.php
@@ -69,18 +69,18 @@ if (isset($_GET['logout'])) {
 <nav class ="navbar navbar-custom">
     <div class ="container-fluid navgationbar">
         <div class ="navbar-header logo">
-            <a href="index.php"><img src="blueLogo.png" width="90" height="70"/></a>
+            <a href="../../../index.php"><img src="blueLogo.png" width="90" height="70"/></a>
         </div>
         <div class="nav navbar-nav navbar-left">
-            <li><a href="index.php">HOME</a></li>
+            <li><a href="../../../index.php">HOME</a></li>
             <li><a href="../../../aboutus.php">ABOUT</a></li>
         </div>
         <div>
             <ul class="nav navbar-nav navbar-right">
                 <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"> JOIN US <span class="caret"></span></a>
                     <ul class="dropdown-menu">
-                        <li><a href="app/views/home/user_registration.php">User</a></li>
-                        <li><a href="app/views/home/restaurant_registration.php">Restaurant</a></li>
+                        <li><a href="user_registration.php">User</a></li>
+                        <li><a href="restaurant_registration.php">Restaurant</a></li>
                     </ul>
                 </li>             
                 <?php
@@ -89,23 +89,23 @@ if (isset($_GET['logout'])) {
                     if (isset($_SESSION['role'])) {
                         switch ($_SESSION['role']) {
                             case 'owner':
-                                $link = 'app/views/owner/ownerpage.php';
+                                $link = '../owner/ownerpage.php';
                                 break;
                             case 'user':
-                                $link = 'app/views/user/userpage.php';
+                                $link = '../user/userpage.php';
                                 break;
                             case 'host':
-                                $link = 'app/views/host/hostview.php';
+                                $link = '../host/hostview.php';
                                 break;
                             case 'admin':
-                                $link = 'app/views/admin/adminpage.php';
+                                $link = '../admin/adminpage.php';
                                 break;
                         }
                     }
                     echo '<li><a href=' . $link . '>My Profile</a></li>';
                     echo '<li><a href="?logout=1">Logout</a></li>';
                 } else {
-                    echo '<li> <a href ="app/views/home/login.php">LOGIN</a></li>';
+                    echo '<li> <a href ="login.php">LOGIN</a></li>';
                 }
                 ?>
             </ul>

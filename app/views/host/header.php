91b5a9d45d8ddc8f7f123f7176126547f2de98d8
mlee8 <openmichael65@gmail.com>
2015-12-12 05:56:57 +0000

    Update all the header file

--- app/views/host/header.php
+++ app/views/host/header.php
@@ -2,6 +2,7 @@
 if (session_id() == '') {
     session_start();
 }
+
 if (isset($_GET['logout'])) {
     unset($_SESSION['username']);
     if (isset($_SESSION['user_id'])) {
@@ -10,19 +11,78 @@ if (isset($_GET['logout'])) {
     unset($_GET['logout']);
 }
 ?>
-<nav class ="navbar navbar-default">
-    <div class ="container-fluid">
-        <div class ="navbar-header">
-            <a href="../../../index.php"><img src="../../../blueLogo.png"  width="175" height="120"/></a>
+
+<style>
+    h1 {
+        text-align: center;
+    }
+    h2 {
+        text-align: center;
+    }
+    .member {
+        text-align: center;
+    }
+    h5{
+        font-weight: bold;
+    }
+
+    .member{
+        margin: 10px 10px 10px 10px;
+        border-radius: 25px;
+        border: 1px solid #e3e3e3;
+        background-color:#f5f5f5;
+        /*background-color:rgba(0,0,0,0.5)*/
+    }
+    .navbar-custom {
+        margin-bottom: 0px;
+        width: 100%;
+        height: 80px;
+        font-size: 120%;
+        font-weight: bold;
+        background-color: #F0FFFF;
+    }
+
+    li a {           
+        color: #000000;
+    }
+
+    .logo{
+        padding-top: 5px;
+        padding-right: 15px;
+    }
+
+    .navbar-left {
+        padding-top: 15px;
+    }
+
+    .navbar-right {
+        padding-top: 15px
+    }
+    
+    .navgationbar{
+        background-color:rgba(0,0,0,0)!important;
+        border: none!important;
+        margin: 0 0 0 0!important;
+    }
+</style>
+
+<nav class ="navbar navbar-custom">
+    <div class ="container-fluid navgationbar">
+        <div class ="navbar-header logo">
+            <a href="../../../index.php"><img src="blueLogo.png" width="90" height="70"/></a>
+        </div>
+        <div class="nav navbar-nav navbar-left">
+            <li><a href="../../../index.php">HOME</a></li>
+            <li><a href="../../../aboutus.php">ABOUT</a></li>
         </div>
         <div>
             <ul class="nav navbar-nav navbar-right">
-                <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#"> Register <span class="caret"></span></a>
+                <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"> JOIN US <span class="caret"></span></a>
                     <ul class="dropdown-menu">
-                        <li><a href="../home/user_registration.php"> User</a></li>
-                            <li><a href="../home/restaurant_registration.php">Restaurant</a></li>
+                        <li><a href="../home/user_registration.php">User</a></li>
+                        <li><a href="../home/restaurant_registration.php">Restaurant</a></li>
                     </ul>
-                </li>
+                </li>             
                 <?php
                 if (isset($_SESSION['username'])) {
                     $link = "#";
@@ -45,10 +105,9 @@ if (isset($_GET['logout'])) {
                     echo '<li><a href=' . $link . '>My Profile</a></li>';
                     echo '<li><a href="?logout=1">Logout</a></li>';
                 } else {
-                    echo '<li> <a href ="../home/login.php">Login</a></li>';
+                    echo '<li> <a href ="../home/login.php">LOGIN</a></li>';
                 }
                 ?>
-
             </ul>
         </div>
     </div>
